2f3abea154f68af604c4e04bf23fde0bbc1051a7
Nakul102 <nakulsomani02@gmail.com>
2024-10-26 15:07:02 +0530

    second commit

--- src/index.js
+++ src/index.js
@@ -4,9 +4,9 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-// Replace with your actual Auth0 domain and client ID
-const domain = 'dev-ah4n30a7wqzo2izq.us.auth0.com'; // e.g., 'myapp.auth0.com'
-const clientId = 'TIQtvfktQ8OCMv0wU2BIxKilvMTIsqmT'; // e.g., 'abc123'
+
+const domain = 'dev-ah4n30a7wqzo2izq.us.auth0.com'; 
+const clientId = 'TIQtvfktQ8OCMv0wU2BIxKilvMTIsqmT'; 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
